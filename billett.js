4990a0fd71512ca4a85995756147f77cb7ef68e8
saabd6875 <saabd6875@oslomet.no>
2024-02-13 15:19:06 +0100

    updated html and js

--- billett.js
+++ billett.js
@@ -3,27 +3,42 @@
 //document.getElementById("resultat").innerHTML=ut;
 
 let objArray= [];
-function addToArray(){
+function addToArray() {
     console.log("button clicked");
+    let film = document.getElementById("film").value;
+    let antall = document.getElementById("quantity").value;
     let fname = document.getElementById("fname").value;
     let lname = document.getElementById("lname").value;
     let phonenr = document.getElementById("phonenr").value;
     let mail = document.getElementById("mail").value;
-    objArray.push({fnameKey : fname, lnameKey : lname, phonenrKey : phonenr, mailKey : mail});
+    objArray.push({
+        filmkey: film,
+        antallkey: antall,
+        fnameKey: fname,
+        lnameKey: lname,
+        phonenrKey: phonenr,
+        mailKey: mail
+    });
     console.log(objArray);
     populateHTML(objArray);
 
-}
 
+}
 function populateHTML(objArr){
     console.log("array")
     let html = "<ol>";
     console.log(objArr)
     for (let i in objArr){
         console.log(objArr[i].fnameKey)
-        html += "<li>" + objArr[i].fnameKey + " " + objArr[i].lnameKey + " " + objArr[i].phonenrKey + " " + objArr[i].mailKey + "</li>";
+        html += "<li>" +  objArr[i].filmkey +" " + objArr[i].antallkey + " " +  objArr[i].fnameKey +
+            " " + objArr[i].lnameKey + " " + objArr[i].phonenrKey + " " + objArr[i].mailKey + "</li>";
     }
     html += "</ol>"
     document.getElementById("resultObject").innerHTML = html;
     console.log(html)
+}
+
+function SlettArray(){
+    objArray = [];
+    populateHTML(objArray);
 }
